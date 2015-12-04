2eb18d9927db74032026a8c06199a449806581c4
bob <bob@example.com>
2015-12-04 11:47:48 +0000

    added more blurb to tfl_config.py

--- tfl_config.py
+++ tfl_config.py
@@ -1,8 +1,7 @@
 """
 The TfL API rate-limits the usage of unregistered users.
-For this reason, you should register for an API ID and key at https://api-portal.tfl.gov.uk/signup . Once you have done so, edit the below definitions of APP_ID and APP_KEY to use the API as a registered user.
-
-
+For this reason, you should register for an API ID and key at https://api-portal.tfl.gov.uk/signup .
+Once you have done so, edit the below definitions of APP_ID and APP_KEY to use the API as a registered user.
 """
 
 APP_ID = ""
