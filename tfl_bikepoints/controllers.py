e6a2d0522ebd00e358675ed30e386d931e654152
Bob Webb <bob.wbb@googlemail.com>
2015-12-17 00:41:08 +0000

    simplified controller a bit

--- tfl_bikepoints/controllers.py
+++ tfl_bikepoints/controllers.py
@@ -21,12 +21,12 @@ def get_auth_from_environ():
     app_key = os.environ.get('APP_KEY',"")
     return app_id, app_key
 
-def extract_marker_data(bikepoints):
+def extract_marker_json(bikepoints):
     """
-    This method returns a simple python data structure that can
-    then be encoded as JSON to be displayed by Leaflet.
+    This method returns a JSON representation of the bikepoints,
+    to be displayed by Leaflet
     """
-    return [bp.to_marker_data() for bp in bikepoints]
+    return json.dumps([bp.to_marker_data() for bp in bikepoints])
 
 def get_last_edited():
     m = db.session.query(Meta).first()
@@ -94,8 +94,7 @@ def index(error=""):
 
     bikepoints = db.session.query(BikePoint).all()
 
-    marker_data = extract_marker_data(bikepoints)
-    marker_data_json = json.dumps(marker_data)
+    marker_data_json = extract_marker_json(bikepoints)
 
     return render_template('bikepoint_list.html', bikepoints=bikepoints,
                            marker_data=marker_data_json,
@@ -117,8 +116,7 @@ def search_bikepoints():
         ilike_q = "%{}%".format(query)
         bikepoints = BikePoint.query.filter(BikePoint.name.ilike(ilike_q)).all()
 
-        marker_data = extract_marker_data(bikepoints)
-        marker_data_json = json.dumps(marker_data)
+        marker_data_json = extract_marker_json(bikepoints)
 
         return render_template('bikepoint_list.html',
                                bikepoints=bikepoints,
