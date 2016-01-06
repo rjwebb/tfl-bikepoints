fcdd16f472040ba409abeff6d97bc595a8324ae3
Bob Webb <bob.wbb@googlemail.com>
2016-01-06 00:03:33 +0000

    made a TestProject class that contains the setUp method

--- test.py
+++ test.py
@@ -6,19 +6,22 @@ from tfl_bikepoints.models import Meta
 #import tfl_bikepoints.controllers as controllers
 # Uses code from: kronosapiens.github.io/blog/2014/07/29/setting-up-unit-tests-with-flask.html
 
+# Uses code from:
+# kronosapiens.github.io/blog/2014/07/29/setting-up-unit-tests-with-flask.html
 
-class TestMeta(unittest.TestCase):
+class TestProject(unittest.TestCase):
     def setUp(self):
         app.config.from_object('tfl_bikepoints.config.TestingConfig')
         db.session.close()
         db.drop_all()
         db.create_all()
 
+
+class TestMeta(TestProject):
     def test_initial_last_edited_no_data(self):
         all_meta = db.session.query(Meta).all()
         assert len(all_meta) == 0
         assert Meta.get_last_edited() == None
-
 
     def test_set_last_edited(self):
         # test it once
